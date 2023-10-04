63333615eebda125cdf91e845eacc1350c878f8a
MCGom <100755492+MCGom@users.noreply.github.com>
2023-10-04 23:03:49 +0900

    툰테이블 쿼리문 확정 및 네이버 크롤러 확정

--- Toon_table_Create.sql
+++ Toon_table_Create.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `toon_table` (
-  `toon_num` int NOT NULL,
-  `N_toon` varchar(105) NOT NULL,
+  `Toon_idx` int NOT NULL,
+  `Toon_name` varchar(105) NOT NULL,
   `Toon_category` varchar(105) DEFAULT NULL,
   `Toon_link` varchar(300) DEFAULT NULL,
   `Toon_update` int DEFAULT NULL,
   `Toon_newupdate` int DEFAULT NULL,
-  `Toon_Hit_toon` int DEFAULT NULL,
+  `Toon_exist` int DEFAULT '0',
   `Toon_imagelink` varchar(300) DEFAULT NULL,
   `Toon_platform` int DEFAULT NULL,
   `Toon_likes` int DEFAULT NULL,
-  PRIMARY KEY (`toon_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+  PRIMARY KEY (`Toon_idx`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
